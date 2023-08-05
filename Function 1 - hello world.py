50c81af5c2020e4847a16701ef27313824b90059
RDenisA <129385620+RDenisA@users.noreply.github.com>
2023-08-05 22:01:09 +0300

    Create Function 1 - hello world.py

---
+++ Function 1 - hello world.py
@@ -0,0 +1,9 @@
+# Write a function `greet` that returns "hello world!"
+def greet():
+    return "hello world!"
+
+
+
+
+#  Make a simple function called greet
+#  that returns the most-famous "hello world!".
